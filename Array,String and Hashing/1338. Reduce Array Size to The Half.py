567c3fa38c9282dbeeb5469fb40b65888ea2ea2e
nvnxkr <nvnkr2005@gmail.com>
2026-04-11 00:32:51 +0530

    11th April

--- Array,String and Hashing/1338. Reduce Array Size to The Half.py
+++ Array,String and Hashing/1338. Reduce Array Size to The Half.py
@@ -1,5 +1,45 @@
+'''
+
+You are given an integer array arr. You can choose a set of integers and remove all the occurrences of these integers in the array.
+Return the minimum size of the set so that at least half of the integers of the array are removed.
+
+Example 1:
+Input: arr = [3,3,3,3,5,5,5,2,2,7]
+Output: 2
+Explanation: Choosing {3,7} will make the new array [5,5,5,2,2] which has size 5 (i.e equal to half of the size of the old array).
+Possible sets of size 2 are {3,5},{3,2},{5,2}.
+Choosing set {2,7} is not possible as it will make the new array [3,3,3,3,5,5,5] which has a size greater than half of the size of the old array.
+
+Example 2:
+Input: arr = [7,7,7,7,7,7]
+Output: 1
+Explanation: The only possible set you can choose is {7}. This will make the new array empty.
+'''
+
 from collections import Counter
 
-arr=[1,1,1,2,2,3,1,2,3,1]
+from typing import List
+
+def minSetSize(arr: List[int]) -> int:
+    
+    freq=Counter(arr)
+
+    count=sorted(freq.values(),reverse=True)
+
+    ans=0
+    removed=0
+
+    for ch in count:
+        removed+=ch
+        ans+=1
+
+        if removed>=len(arr)//2:
+            return ans
+
+arr = [3,3,3,3,5,5,5,2,2,7]
+print(minSetSize(arr))
 
 
+        
+
+        
